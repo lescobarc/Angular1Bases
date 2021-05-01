3e611e82baafab39ef25119004b3804cbe1de688
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-01 18:22:10 -0500

    S4 - 36 Contador App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: //'<span>Laura</span>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App';
+  numero: number = 10;
 }
